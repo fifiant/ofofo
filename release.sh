e0c7ce1c2e7417d3cbb581b991b2723875dc90b7
Fawaz PARAISO <fifiant@gmail.com>
2014-02-27 20:36:53 +0100

    Update
    Update release file.

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Do maven release, then push the release created in the maven_repo project.
-mvn -X release:clean release:prepare release:perform && ../maven-repo/push_release.sh && mvn -X clean && git push 
+mvn -X release:clean release:prepare release:perform && ../maven_repo/push_release.sh && mvn -X clean && git push 
